8c3b99d03bb5ef1656950f15d047c6bcee495be6
JainerPinta <95033872+JainerPinta@users.noreply.github.com>
2022-01-02 23:40:54 -0500

    Add files via upload

---
+++ EjerciciosASM/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "secureip" -o "/home/ise/Desktop/Proyectos/EjerciciosASM/TB_Alarma_isim_beh.exe" -prj "/home/ise/Desktop/Proyectos/EjerciciosASM/TB_Alarma_beh.prj" "work.TB_Alarma" 
